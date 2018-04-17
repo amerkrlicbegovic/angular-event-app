d571b036564313e67e2357be1b57b2d5745da9a5
Amer <amer.krlicbegovic@gmail.com>
2018-04-17 08:38:04 +0200

    Project Angular Event app - adding event service for event list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail..component';
 import { NavBarComponent } from './nav/navbar.component';
-
+import { EventService } from './events/shared/event.service'
 
 @NgModule({
   imports: [
@@ -18,6 +18,7 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
